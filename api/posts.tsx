aa38cc954bbb6bfbeecbaf77fd99b09e96ba1385
ofcorp <director@demrev.biz>
2026-08-21 00:12:09 +0300

    3-ssr-1 dz ispr1

--- api/posts.tsx
+++ api/posts.tsx
@@ -7,5 +7,6 @@ export async function getPosts(): Promise<IPost[]> {
     headers: new Headers({ 'content-type': 'application/json' }),
     next: { revalidate: 10 },
   });
+  if (!res.ok) throw new Error(`Failed to fetch posts, status: ${res.status}`);
   return res.json();
 }
